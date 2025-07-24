f04bb8ce0b5e5ebe0fa909e7aa967fa2bf8d1cae
Gleb kolobkov <gleyba.k@gmail.com>
2025-07-24 00:44:37 -0700

    😻 QT example works!!! `bazel run //:hello_qt` and `Hello world!` button popup! Just need few polishing actions to mark it done for MacOSX arm64!

--- examples/hello_qt/hello_qt.cpp
+++ examples/hello_qt/hello_qt.cpp
@@ -1,13 +1,12 @@
+#include <QtCore/QtPlugin>
 #include <QtWidgets/QApplication>
 #include <QtWidgets/QPushButton>
 
 #include <iostream>
 
+Q_IMPORT_PLUGIN(QCocoaIntegrationPlugin);
+
 int main(int argc, char *argv[]) {
-    std::cout << std::getenv("QT_QPA_PLATFORM_PLUGIN_PATH") << std::endl;
-
-    std::system("ls -la ../rules_vcpkg++vcpkg+vcpkg/qt6_plugins/");
-
     QApplication a(argc, argv);
     QPushButton button ("Hello world!");
     button.show();
